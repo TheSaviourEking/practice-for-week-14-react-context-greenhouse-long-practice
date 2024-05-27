436637aee453041eaa3f94b4c6da5da25dc7db0d
TheSaviourEking <saviour.eking@outlook.com>
2024-05-27 02:13:18 +0100

    chore: implement Bonus Phase 1: Thermostat

--- src/components/Thermometer/Thermometer.js
+++ src/components/Thermometer/Thermometer.js
@@ -2,18 +2,35 @@ import ReactSlider from "react-slider";
 import './Thermometer.css';
 
 import { useClimate } from "../../context/ClimateContext";
+import { useEffect, useState } from "react";
 
 function Thermometer() {
   const { temperature, setTemperature } = useClimate();
-  
+  let [defaultTemperature, setDefaultTemperature] = useState(temperature);
+
+  useEffect(() => {
+    if (defaultTemperature !== temperature) {
+      const intervalId = setInterval(() => {
+        setTemperature(prevTemp => {
+          if (prevTemp < defaultTemperature) return prevTemp + 1;
+          if (prevTemp > defaultTemperature) return prevTemp - 1;
+          clearInterval(intervalId);
+          return prevTemp;
+        })
+      }, 1000);
+      setInterval(intervalId);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [defaultTemperature, temperature])
+
   return (
     <section>
       <h2>Thermometer</h2>
-      <div className="actual-temp">Actual Temperature: {"x"}°F</div>
+      <div className="actual-temp">Actual Temperature: {temperature}°F</div>
       <ReactSlider
-        value={temperature}
-        // onAfterChange={(val) => { }}
-        onAfterChange={(val) => { setTemperature(() => val) }}
+        value={defaultTemperature}
+        onAfterChange={(val) => { setDefaultTemperature(() => val) }}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
