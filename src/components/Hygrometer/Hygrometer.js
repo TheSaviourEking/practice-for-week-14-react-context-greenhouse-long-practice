b9e086675e248e3c2e47367b8c8ed1987f317a2c
TheSaviourEking <saviour.eking@outlook.com>
2024-05-27 02:22:25 +0100

    chore: implement Bonus Phase 2: Hygrometer

--- src/components/Hygrometer/Hygrometer.js
+++ src/components/Hygrometer/Hygrometer.js
@@ -2,17 +2,36 @@ import ReactSlider from "react-slider";
 import "./Hygrometer.css";
 
 import { useClimate } from "../../context/ClimateContext";
+import { useEffect, useState } from "react";
 
 function Hygrometer() {
   const { humidity, setHumidity } = useClimate();
+  const [defaultHumidity, setDefaultHumidity] = useState(humidity);
+
+  useEffect(() => {
+    if (humidity !== defaultHumidity) {
+      const intervalId = setTimeout(() => {
+        setHumidity(prevHumidity => {
+          if (prevHumidity < defaultHumidity) prevHumidity++;
+          else if (prevHumidity > defaultHumidity) prevHumidity--;
+          else {
+            clearTimeout(intervalId)
+          }
+          return prevHumidity;
+        })
+        setTimeout(intervalId);
+        return () => clearTimeout(intervalId);
+      }, 1000)
+    }
+  }, [humidity, defaultHumidity])
 
   return (
     <section>
       <h2>Hygrometer</h2>
-      <div className="actual-humid">Actual Humidity: {"y"}%</div>
+      <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={humidity}
-        onAfterChange={(val) => { setHumidity(() => val) }}
+        value={defaultHumidity}
+        onAfterChange={(val) => { setDefaultHumidity(() => val) }}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
